60a27c2fb90580e4d099f77ff924755cd0f30d41
Ukashwaran <80847521+ukashremohub@users.noreply.github.com>
2022-01-25 12:59:58 +0530

    Add files via upload

---
+++ Test2/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Test2
+{
+    class Program
+    {
+       
+       public static void Main(string[] args)
+        {
+            StudentList studentList = new StudentList();
+            /* studentList.AddStudent(new Student()
+             {
+                 id = 1,
+                 age = 22,
+                 marks = 85,
+                 name = "ukash"
+             });*/
+            studentList.AddStudent();
+            studentList.GetStudentList();
+            Console.WriteLine(studentList.GetStudentByID(1));
+            Console.WriteLine(studentList.UpdateElementById(1,"waran",24,66));
+            studentList.DeleteById(1);
+            studentList.GetStudentList();
+
+
+
+
+           /* List<Student> MyStudents = new List<Student>();            
+            MyStudents.Add(new Student()
+            {
+                id = 1,
+                age = 22,
+                marks = 85,
+                name = "ukash"
+            });
+
+            MyStudents.Add(new Student());
+            MyStudents[1].id = 2;
+            MyStudents[1].age = 21;
+            MyStudents[1].marks = 61;
+            MyStudents[1].name = "queen";
+            
+            foreach (Student stu in MyStudents)
+            {
+                Console.WriteLine(stu);
+            }*/
+
+          /*  //get
+            Console.WriteLine("Enter id to get values");
+            int Get_value =Convert.ToInt32(Console.ReadLine());
+            Student get = MyStudents.Find(x => x.id == Get_value);
+            Console.WriteLine(get.Get());
+
+            //update
+            Console.WriteLine("Enter id to update values");
+            int upd_value = Convert.ToInt32(Console.ReadLine());
+            Student upd = MyStudents.Find(x => x.id == upd_value);
+            upd.name
+            foreach (Student stu in MyStudents)
+            {
+                Console.WriteLine(stu);
+            }
+
+            //del
+            Console.WriteLine("Enter id to delete");
+            int del_value = Convert.ToInt32(Console.ReadLine());
+            Student del = MyStudents.Find(x => x.id == del_value);
+            MyStudents.Remove(del);
+            foreach (Student stu in MyStudents)
+            {
+                Console.WriteLine(stu);
+            }*/
+        }
+
+    }
+    
+}
---
+++ Test2/StudentList.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Test2
+{
+    public class StudentList
+    {
+        private List<Student> studentlist = new List<Student>();
+
+        /* public List<Student> AddStudent(Student student)
+         {
+             this.studentlist.Add(student);
+             return this.studentlist;
+         }*/
+
+        public List<Student> AddStudent()
+        {
+            this.studentlist.Add(new Student()
+            {
+                id = 1,
+                age = 22,
+                marks = 85,
+                name = "ukash"
+            });
+            this.studentlist.Add(new Student()
+            {
+                id = 2,
+                age = 22,
+                marks = 61,
+                name = "queen"
+            });
+            return this.studentlist;
+        }
+        public void GetStudentList()
+        {
+            foreach (Student stu in studentlist)
+            {
+                Console.WriteLine(stu);
+            }
+           // return this.studentlist;
+        }
+
+        public Student GetStudentByID(int id)
+        {
+            return this.studentlist.Find(x => x.id == id);
+        }
+
+        public void DeleteById(int id)
+        {
+            int index = studentlist.FindIndex(x => x.id == id);
+            studentlist.RemoveAt(index);
+        }
+
+        public Student UpdateElementById(int id, string name, int age, int marks)
+        {
+            Student delete = studentlist.Find(x => x.id == id);
+            delete.name = name;
+            delete.age = age;
+            delete.marks = marks;
+            return delete;
+        }
+    }
+}
